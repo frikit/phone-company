349bd09ca7160f2964d9c39e9d6892501bfc032b
frikit <osipovvictor1994@gmail.com>
2021-02-23 12:00:38 +0000

    functional style PromotionService

--- src/main/scala/com/phone/services/PromotionService.scala
+++ src/main/scala/com/phone/services/PromotionService.scala
@@ -3,13 +3,24 @@ package com.phone.services
 import com.phone.models.{CallPrice, CustomerCall}
 import com.phone.promotions.Promotion
 
+import scala.annotation.tailrec
+
 class PromotionService {
 
-  def applyPromotions(calls: Map[CustomerCall, CallPrice], promotions: Promotion*): Map[CustomerCall, CallPrice] = {
-    var r = calls
-    for(p <- promotions) {
-      r = p.applyPromotion(r)
+  @tailrec
+  private def applyPromotion(
+                              calls: Map[CustomerCall, CallPrice],
+                              promotion: Promotion,
+                              promotions: Seq[Promotion]
+                            ): Map[CustomerCall, CallPrice] = {
+    if (promotions.isEmpty) {
+      promotion.applyPromotion(calls)
+    } else {
+      applyPromotion(promotion.applyPromotion(calls), promotions.head, promotions.tail)
     }
-    r
+  }
+
+  def applyPromotions(calls: Map[CustomerCall, CallPrice], promotions: Promotion*): Map[CustomerCall, CallPrice] = {
+    applyPromotion(calls, promotions.head, promotions.tail)
   }
 }
